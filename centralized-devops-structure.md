18e8796e3da0c54f340ac20f2d577e55dfbbc853
Ishmael Gyamfi <ishmael.gyamfi.dev@gmail.com>
2025-10-17 16:06:17 +0000

    change varoable name from AWS_DEFAULT_REGION to AWS_REGION

--- centralized-devops-structure.md
+++ centralized-devops-structure.md
@@ -187,7 +187,7 @@ Monitoring: full-stack
 AWS:
   - AWS_ACCESS_KEY_ID
   - AWS_SECRET_ACCESS_KEY
-  - AWS_DEFAULT_REGION
+  - AWS_REGION
 
 Container Registry:
   - ECR_REGISTRY_URL
--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -14,3 +14,13 @@ output "ecs_task_role_arns" {
     event_service = aws_iam_role.event_service_task.arn
   }
 }
+
+output "github_actions_role_arn" {
+  description = "ARN of the GitHub Actions IAM role"
+  value       = aws_iam_role.github_actions.arn
+}
+
+output "github_oidc_provider_arn" {
+  description = "ARN of the GitHub OIDC provider"
+  value       = aws_iam_openid_connect_provider.github.arn
+}
