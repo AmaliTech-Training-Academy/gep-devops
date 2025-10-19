12e991e0ff8ced75ddda071c9f09b612769a5ea0
mangucletus <mangucletus@gmail.com>
2025-10-19 08:59:54 +0000

    disable acm to avoid validation timeout

--- terraform/modules/acm/outputs.tf
+++ terraform/modules/acm/outputs.tf
@@ -1,30 +1,30 @@
-# terraform/modules/acm/outputs.tf
-output "alb_certificate_arn" {
-  description = "ARN of the ALB certificate"
-  value       = var.create_alb_certificate ? aws_acm_certificate.alb[0].arn : null
-}
+# # terraform/modules/acm/outputs.tf
+# output "alb_certificate_arn" {
+#   description = "ARN of the ALB certificate"
+#   value       = var.create_alb_certificate ? aws_acm_certificate.alb[0].arn : null
+# }
 
-output "alb_certificate_status" {
-  description = "Status of the ALB certificate"
-  value       = var.create_alb_certificate ? aws_acm_certificate.alb[0].status : null
-}
+# output "alb_certificate_status" {
+#   description = "Status of the ALB certificate"
+#   value       = var.create_alb_certificate ? aws_acm_certificate.alb[0].status : null
+# }
 
-output "alb_certificate_domain_validation_options" {
-  description = "Domain validation options for ALB certificate"
-  value       = var.create_alb_certificate ? aws_acm_certificate.alb[0].domain_validation_options : null
-}
+# output "alb_certificate_domain_validation_options" {
+#   description = "Domain validation options for ALB certificate"
+#   value       = var.create_alb_certificate ? aws_acm_certificate.alb[0].domain_validation_options : null
+# }
 
-output "cloudfront_certificate_arn" {
-  description = "ARN of the CloudFront certificate"
-  value       = var.create_cloudfront_certificate ? aws_acm_certificate.cloudfront[0].arn : null
-}
+# output "cloudfront_certificate_arn" {
+#   description = "ARN of the CloudFront certificate"
+#   value       = var.create_cloudfront_certificate ? aws_acm_certificate.cloudfront[0].arn : null
+# }
 
-output "cloudfront_certificate_status" {
-  description = "Status of the CloudFront certificate"
-  value       = var.create_cloudfront_certificate ? aws_acm_certificate.cloudfront[0].status : null
-}
+# output "cloudfront_certificate_status" {
+#   description = "Status of the CloudFront certificate"
+#   value       = var.create_cloudfront_certificate ? aws_acm_certificate.cloudfront[0].status : null
+# }
 
-output "cloudfront_certificate_domain_validation_options" {
-  description = "Domain validation options for CloudFront certificate"
-  value       = var.create_cloudfront_certificate ? aws_acm_certificate.cloudfront[0].domain_validation_options : null
-}
+# output "cloudfront_certificate_domain_validation_options" {
+#   description = "Domain validation options for CloudFront certificate"
+#   value       = var.create_cloudfront_certificate ? aws_acm_certificate.cloudfront[0].domain_validation_options : null
+# }
