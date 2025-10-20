4975aa53ff902f648bf3d5aeb08737482bc0e528
mangucletus <mangucletus@gmail.com>
2025-10-20 16:22:12 +0000

    modify auth-token length in elasticache module

--- terraform/modules/elasticache/variables.tf
+++ terraform/modules/elasticache/variables.tf
@@ -191,16 +191,13 @@ variable "auth_token" {
 
   validation {
     condition = (
-      var.auth_token == null
-      || (
-        can(length(tostring(var.auth_token)))
-        && length(tostring(var.auth_token)) >= 16
-        && length(tostring(var.auth_token)) <= 128
-      )
+      var.auth_token == null ||
+      (try(length(var.auth_token), 0) >= 16 && try(length(var.auth_token), 0) <= 128)
     )
     error_message = "Auth token must be null or between 16 and 128 characters if provided."
   }
 }
+
 
 
 # ==============================================================================
