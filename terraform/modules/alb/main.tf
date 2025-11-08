5a628d12a0d31e36b081105947a6d04beff2fea2
mangucletus <mangucletus@gmail.com>
2025-11-08 16:19:25 +0000

    Add lifecycle block to prevent random_password from regenerating on every apply

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -252,10 +252,6 @@ resource "aws_lb_target_group" "services" {
     cookie_duration = 86400
   }
 
-  # Preserve host header and enable CORS
-  preserve_client_ip = false
-  proxy_protocol_v2  = false
-
   tags = merge(
     local.common_tags,
     {
